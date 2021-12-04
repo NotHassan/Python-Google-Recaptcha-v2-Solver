3adaff078e9aa9be0a2aa46d68ccab8254ef0c4d
Hassan Mohammad <hassanmoha0000@gmail.com>
2021-12-03 22:04:04 -0500

    Update solve_recaptcha_demo.py

--- solve_recaptcha_demo.py
+++ solve_recaptcha_demo.py
@@ -5,9 +5,10 @@ import time
 print('Started solve_recaptcha_demo.py')
 start_time = time.time()
 gcaptcha = Gcaptcha('https://www.google.com/recaptcha/api2/demo')
+end_time = time.time()
 print('Google Recaptcha response: {}'.format(gcaptcha.response))
 
-print("--- %s seconds ---" % (time.time() - start_time))
+print("--- %s seconds ---" % (end_time - start_time))
 print('Transcription Attempts: {}'.format(gcaptcha.transcription.attempts))
 print('Transcription Successes: {}'.format(gcaptcha.transcription.successful))
 print('Transcription Fails: {}'.format(gcaptcha.transcription.failed))
